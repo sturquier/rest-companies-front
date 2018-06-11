02fac36bdc71b40177e679dbdf5f5225df33990d
Simon Turquier <turquiersimon@hotmail.fr>
2018-06-11 13:52:28 +0200

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          To get started, edit <code>src/App.js</code> and save to reload.
+      </div>
+    );
+  }
+}
+
+export default App;
