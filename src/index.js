33941ec586dd92b20c5b1c66007079ad535d802f
Simon Turquier <turquiersimon@hotmail.fr>
2018-06-11 14:21:20 +0200

    Setup reducers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 registerServiceWorker();
